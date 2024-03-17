4a68129be3d121325b1c1964cb0122bb7b3b8c2b
Alipa <alipakkr24@gmail.com>
2024-03-17 19:01:15 +0530

    backend done

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,9 @@ app.all("*", (req, res) => {
    res.status(404).send("404 NOT FOUND");
    
 });
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
 
 // enabling socket server // -------
 const socketServer = require("http").Server(app);
